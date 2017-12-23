3c7fb37664b0ecfc2915f93f5b5784ae2c62a165
murto <murto@airmail.cc>
2017-12-23 20:45:39 +0000

    Format changes

--- src/logger.lua
+++ src/logger.lua
@@ -1,31 +1,36 @@
 local logger = {}
 
 function logger:new(file_path)
+	assert(file_path, "file_path cannot be nil")
 	local l = {}
 	setmetatable(l, self)
 	self.__index = self
 	local msg = nil
-	local file, msg = io.open(file_path, "w")
+	local file, reason = io.open(file_path, "w")
 	if (not file) then
-		error(msg)
+		error(reason)
 	end
 	l.file = file
 	return l
 end
 
 function logger:log_success(msg)
-	self.file:write(string.char(27) .. "[32mSUCCESS:\n    " .. msg .. string.char(27) .. "[0m\n")
+	assert(msg, "msg cannot be nil")
+	self.file:write(string.char(27) .. "[32mSUCCESS" .. string.char(27) .. "[0m: " .. msg .. '\n')
 end
 
 function logger:log_warning(msg)
-	self.file:write(string.char(27) .. "[33mERROR:\n    " .. msg .. string.char(27) .. "[0m\n")
+	assert(msg, "msg cannot be nil")
+	self.file:write(string.char(27) .. "[33mWARNING" .. string.char(27) .. "[0m: " .. msg .. '\n')
 end
 
 function logger:log_error(msg)
-	self.file:write(string.char(27) .. "[31mERROR:\n    " .. msg .. string.char(27) .. "[0m\n")
+	assert(msg, "msg cannot be nil")
+	self.file:write(string.char(27) .. "[31mERROR" .. string.char(27) .. "[0m: " .. msg .. '\n')
 end
 
 function logger:log_message(msg)
+	assert(msg, "msg cannot be nil")
 	self.file:write(msg .. '\n')
 end
 
