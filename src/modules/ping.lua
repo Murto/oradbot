c44ae8715ece0f140f0889bcc26da3f6076d84fd
murto <murto@airmail.cc>
2017-12-25 02:45:58 +0000

    Move around elements

--- src/modules/ping.lua
+++ src/modules/ping.lua
@@ -2,12 +2,18 @@ local command = require("command")
 local embed = require("embed")
 local module = require("module")
 
-local name = "Ping"
-local desc = "A utility to test bot responsiveness"
+
+-- Module commands
 
 local ping = command:new("ping", function(msg)
 		local e = embed:new("pong!", 0x00FF00)
 		msg:reply(e)
 	end, 0)
+
+
+-- Module creation
+
+local name = "Ping"
+local desc = "A utility to test bot responsiveness"
 
 return module:new(name, desc, {ping}, nil)
